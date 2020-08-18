c781ca9b7afff2ebd391a1518d7b05b7afe527f7
David Erickson <daviderickson@Davids-MacBook-Pro.local>
2020-08-17 20:54:19 -0400

    Finished lesson 316 - implemented validation method and implemented error messages

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,12 +1,52 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-function StreamCreate() {
-    return (
-        <div>
-            StreamCreate
-        </div>
-    )
+class StreamCreate extends React.Component {
+
+    renderInput({ input, label, meta  }){ //destructured formProps
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input}/>
+                <div>{meta.error}</div>
+            </div>
+   
+
+        )
+    }
+
+    //redux-form handleSubmit prevents default action just passes in values
+    onSubmit(formValues){
+
+    }
+
+    render(){
+        return (
+            <div>
+                <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                    <Field name="title" component={this.renderInput} label="Enter Title"/>
+                    <Field name="description" component={this.renderInput} label="Enter Description"/>
+                    <button className="ui button primary">Submit</button>
+                </form>
+            </div>
+        )
+    }
 }
 
-export default StreamCreate
+const validate= (formValues) => {
+    // console.log(formValues)
+    const errors = {};
+    if(!formValues.title){
+        errors.title = 'You must enter a title'
+    }
+    if(!formValues.description){
+        errors.description = 'You must enter a description'
+    }
+    return errors
+}
+
+export default reduxForm({
+    form: 'streamCreate',
+    validate: validate
+})(StreamCreate)
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
+//hook up redux-form
+import  { reducer as formReducer} from 'redux-form'
 import AuthReducer from './AuthReducer'
 
 
 
 export default combineReducers({
-    auth: AuthReducer
+    auth: AuthReducer,
+    form: formReducer
 })
